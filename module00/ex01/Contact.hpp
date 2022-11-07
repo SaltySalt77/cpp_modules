69c777a9b8a2adbd88b0b89d010b0c578b87edef
김소금 <alexisna77@gmail.com>
2022-11-07 08:10:31 +0000

    [feat] : contact.hpp 파일 추가

---
+++ module00/ex01/Contact.hpp
@@ -0,0 +1,15 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+	std::String	phoneNumber;
+	std::String	firstName;
+	std::String	lastName;
+	std::String	darkestSecret;
+public :
+
+}
+
+#endif
