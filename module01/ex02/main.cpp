4ce5d5cfb0d8fada51633c0f95e06a88157b6100
SaltySalt77 <alexisna@naver.com>
2022-11-11 23:31:45 +0900

    [feat] : module 01 ex02 끝냄

---
+++ module01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string	brain("HI THIS IS BRAIN");
+	std::string *brainPtr = &brain;
+	std::string &brainRef = brain;
+
+	std::cout << "The memory address of the string variable : " << &brain << std::endl;
+	std::cout << "The memory address held by stringPTR      : " << brainPtr << std::endl;
+	std::cout << "The memory address held by stringREF      : " << &brainRef << std::endl;
+
+	std::cout << "The value of the string variable          : " << brain << std::endl;
+	std::cout << "The value held by stringPTR               : " << brain << std::endl;
+	std::cout << "The value held by stringREF               : " << brain << std::endl;
+}
