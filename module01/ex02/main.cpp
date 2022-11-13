4bda1abdd249752873665a718295737794b12f3a
Hyeon Na <hyna@c5r8s7.42seoul.kr>
2022-11-13 13:19:20 +0900

    [refect] : 푼 문제들 출력값 가독성 높임

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -9,6 +9,8 @@ int main() {
 	std::cout << "The memory address of the string variable : " << &brain << std::endl;
 	std::cout << "The memory address held by stringPTR      : " << brainPtr << std::endl;
 	std::cout << "The memory address held by stringREF      : " << &brainRef << std::endl;
+	
+	std::cout << std::endl;
 
 	std::cout << "The value of the string variable          : " << brain << std::endl;
 	std::cout << "The value held by stringPTR               : " << brain << std::endl;
--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -3,9 +3,19 @@
 int	main() {
 	Harl	harl;
 
+	std::cout << "[DEBUG]   : ";
 	harl.complain("DEBUG");
+	std::cout << std::endl;
+
+	std::cout << "[INFO]    : ";
 	harl.complain("INFO");
+	std::cout << std::endl;
+
+	std::cout << "[WARNING] : ";
 	harl.complain("WARNING");
+	std::cout << std::endl;
+
+	std::cout << "[ERROR]   : ";
 	harl.complain("ERROR");
 
 }
