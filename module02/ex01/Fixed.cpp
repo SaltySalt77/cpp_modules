22364a737970ac4179661714f59fa18aa846c04d
Hyeon Na <hyna@c5r7s2.42seoul.kr>
2022-11-14 17:09:19 +0900

    [docs] : module02 ex01 파일 추가

---
+++ module02/ex01/Fixed.cpp
@@ -0,0 +1,32 @@
+#include "Fixed.hpp"
+
+
+Fixed::Fixed() {
+	std::cout << "Default constructor called" << std::endl;
+	rawBits = 0;
+}
+
+Fixed::Fixed(const Fixed	&fixed) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->rawBits = fixed.rawBits;
+}
+
+Fixed	&Fixed::operator= (const Fixed	&fixed) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->rawBits = fixed.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->rawBits;
+}
+
+void	Fixed::setRawBits(int const	raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->rawBits = raw;
+}
