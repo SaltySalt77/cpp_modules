2a81a1868b8861c3a5a6a48e747f53c0e5f33295
SaltySalt77 <alexisna@naver.com>
2022-11-27 00:29:09 +0900

    [refact] : module05 ex00 grade 예외처리 방식 변경

---
+++ module05/ex01/Bureaucrat.hpp
@@ -0,0 +1,38 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat {
+private:
+	const std::string	name;
+	int				grade;
+public:
+	Bureaucrat();
+	Bureaucrat(std::string	name, int grade);
+	Bureaucrat(const Bureaucrat	&bureaucrat);
+	Bureaucrat	&operator= (const Bureaucrat	&bureaucrat);
+	~Bureaucrat();
+
+	std::string	getName() const;
+
+	int	getGrade() const;
+	void	setGrade(int grade);
+	void	incrementGrade();
+	void	decrementGrade();
+
+	class GradeTooHighException : public std::exception {
+	public :
+		const char * what() const throw();
+	};
+
+	class GradeTooLowException  : public std::exception {
+	public :
+		const char * what() const throw();
+	};
+};
+
+std::ostream &operator << (std::ostream	&out,	const Bureaucrat	&bureaucrat);
+
+#endif
