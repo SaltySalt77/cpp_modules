3f1faaf4b18888d302b55420b217c8421d8d7909
Hyeon Na <hyna@c1r3s1.42seoul.kr>
2022-12-01 12:24:57 +0900

    [feat] : module07 ex00 구현 완료

---
+++ module07/ex00/main.cpp
@@ -0,0 +1,55 @@
+#include "whatever.hpp"
+#include <iostream>
+#include <iomanip>
+
+int main() {
+	{
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+
+	std::cout << std::endl << "================================" << std::endl << std::endl;
+
+	{
+		char a = '2';
+		char b = '3';
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	}
+
+	std::cout << std::endl << "================================" << std::endl << std::endl;
+
+	{
+		float a = 2.0;
+		float b = 3.0;
+		::swap(a, b);
+		std::cout << std::showpoint << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	}
+
+	std::cout << std::endl << "================================" << std::endl << std::endl;
+
+	{
+		double a = 2.0;
+		double b = 3.0;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	}
+
+	return 0;
+}
