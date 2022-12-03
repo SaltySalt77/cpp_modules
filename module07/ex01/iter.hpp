9b6e7e2b30243b1e2e392452e08da63fbbeafe6a
Hyeon Na <hyna@c5r1s4.42seoul.kr>
2022-12-03 16:38:48 +0900

    [feat] : module07 ex01 const 함수도 받을 수 있도록 수정

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -3,16 +3,33 @@
 
 # include <cstddef>
 # include <iostream>
+# include <iomanip>
 
 template <typename T>
-void iter(T	*arr, size_t len, void (*fptr)(T)) {
+void iter(T	*arr, size_t len, void (*fptr)(T&)) {
 		for (size_t i = 0; i < len; i++)
 			fptr(arr[i]);
 }
 
 template <typename T>
-void	print(T a) {
+void iter(const T	*arr, size_t len, void (*fptr)(const T&)) {
+		for (size_t i = 0; i < len; i++)
+			fptr(arr[i]);
+}
+
+template <typename T>
+void	print(const T &a) {
 	std::cout << a << std::endl;
+}
+
+template <typename T>
+void	printTwice(T &a) {
+	std::cout << std::setw(5) << a << " | " << std::setw(5) << a << std::endl;
+}
+
+template <typename T>
+void	squareRoot(T &a) {
+	a = a * a;
 }
 
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,37 +6,47 @@ int main() {
 		const char	*a = test.c_str();
 
 		::iter(a, 2, print);
-		::iter(a, 5, print);
+		::iter(a, 4, printTwice);
+		std::cout << std::endl;
+		std::cout << "===================================================" << std::endl;
 		std::cout << std::endl;
 	}
 	{
 		int	arr[4];
 		for (int i = 0; i < 4; i++)
-			arr[i] = i + 1;
-		::iter(arr, 2, print);
-		::iter(arr, 5, print);
+			arr[i] = i;
+		::iter(arr, 4, squareRoot);
+		::iter(arr, 4, print);
+		std::cout << std::endl;
+		std::cout << "===================================================" << std::endl;
 		std::cout << std::endl;
 	}
 	{
 		float	arr[4];
 		for (int i = 0; i < 4; i++)
-			arr[i] = i + 1;
-		::iter(arr, 2, print);
-		::iter(arr, 5, print);
+			arr[i] = i;
+		::iter(arr, 4, squareRoot);
+		::iter(arr, 4, print);
+		std::cout << std::endl;
+		std::cout << "===================================================" << std::endl;
 		std::cout << std::endl;
 	}
 	{
 		double	arr[4];
 		for (int i = 0; i < 4; i++)
-			arr[i] = i + 1;
-		::iter(arr, 2, print);
-		::iter(arr, 5, print);
+			arr[i] = i;
+		::iter(arr, 4, squareRoot);
+		::iter(arr, 4, print);
+		std::cout << std::endl;
+		std::cout << "===================================================" << std::endl;
 		std::cout << std::endl;
 	}
 	{
 		std::string	arr[4] = {"one", "two", "three", "four" };
 		::iter(arr, 2, print);
-		::iter(arr, 5, print);
+		::iter(arr, 4, printTwice);
+		std::cout << std::endl;
+		std::cout << "===================================================" << std::endl;
 		std::cout << std::endl;
 	}
 	return 0;
